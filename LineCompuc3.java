6e6511e73ec6199feb27131dccbf203e3b6b45c8
Smruti <lenkasmruti123@gmail.com>
2021-09-25 23:58:17 +0530

    Added: LineCompuc3

---
+++ LineCompuc3.java
@@ -0,0 +1,28 @@
+package LineComparison;
+
+public class LineCompuc3 {
+		double distanceCal(int x1,int y1,int x2,int y2)	
+		{
+		
+		         double dis;	
+			     dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)); 	 	    	 	    
+	             return dis;  	         
+		}
+	public static void main(String[] args) {
+		LineCompuc3 obj = new LineCompuc3();
+	   Double line1=obj.distanceCal(1,1,1,1);
+	   Double line2=obj.distanceCal(1,1,1,1);
+	   if(Double.compare(line1,line2)==0)
+	   {
+		   System.out.println("Two lines are equal");
+	   }
+	   else if(Double.compare(line1, line2) < 0)
+	   {
+		   System.out.println("Line1 is less than Line2");
+	   }
+	   else
+		   System.out.println("Line1 is Greater than Line2");
+	}
+	
+
+}
