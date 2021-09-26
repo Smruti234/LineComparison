8fcf84af5b7ee59c7ec5dc4adc52f231fe03354f
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-26 18:53:47 +0530

    Update Lineuc1.java

--- Lineuc1.java
+++ Lineuc1.java
@@ -1,20 +1,28 @@
 package LineComparison;
-
-public class Lineuc1 {
-	
-		double distance(int x1,int y1,int x2,int y2)	
+     
+      public class LineComp {
+		double distanceCal(int x1,int y1,int x2,int y2)	
 		{
 		
 		         double dis;	
-			     dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-			     return dis;
+			     dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)); 	 	    	 	    
+	             return dis;  	         
 		}
 	public static void main(String[] args) {
-	Lineuc1 obj = new Lineuc1();
-	double ans = obj.distance(4,6,7,4);
-	System.out.println("distance between points is :"+ ans);
+		LineCompuc3 obj = new LineCompuc3();
+	   Double line1=obj.distanceCal(1,1,1,1);
+	   Double line2=obj.distanceCal(1,1,1,1);
+	   if(Double.compare(line1,line2)==0)
+	   {
+		   System.out.println("Two lines are equal");
+	   }
+	   else if(Double.compare(line1, line2) < 0)
+	   {
+		   System.out.println("Line1 is less than Line2");
+	   }
+	   else
+		   System.out.println("Line1 is Greater than Line2");
 	}
-		}
-
 	
 
+}
