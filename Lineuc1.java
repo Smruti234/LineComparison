24126bc351fea94b19493e9b0a7335259b313de0
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-26 19:03:11 +0530

    Update Lineuc1.java

--- Lineuc1.java
+++ Lineuc1.java
@@ -1,6 +1,6 @@
 package LineComparison;
 
-public class Lineu {
+public class Line {
 		double distanceCal(int x1,int y1,int x2,int y2)	
 		{
 		
@@ -9,7 +9,7 @@ public class Lineu {
 	             return dis;  	         
 		}
 	public static void main(String[] args) {
-	   Lineuc2 obj = new Lineuc2();
+	   Line obj = new Line();
 	   Double line1=obj.distanceCal(1,1,1,1);
 	   Double line2=obj.distanceCal(2,4,3,2);
 	   System.out.println("line1 and line 2 are equal.True or False :" +line1.equals(line2));
